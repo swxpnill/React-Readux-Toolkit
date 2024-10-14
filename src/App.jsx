4c0be6395ea0c06717c6f77e33d0620ace20d25a
Swapnil <swapnilp161998@gmail.com>
2024-10-14 22:49:15 +0530

    Redux-Toolkit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
